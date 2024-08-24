228a584d2bdeb4cbb9688d40c7ed0c020351ad6a
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-08-25 00:25:45 +0530

    pipelinescript

--- demo.sh
+++ demo.sh
@@ -1,4 +1,4 @@
-echo "$(whoami) is installing ${Component} on ${ENV}"
+echo "$(whoami) is installing ${Component} on ${ENV} with password ${passwd}"
 sudo su -
 touch /home/ec2-user/${Component}.txt
 ls -ltr
